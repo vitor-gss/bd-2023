7fff98c96b45d1944a14919997d94659afdb7f1a
David Lima <david.lima@ifal.edu.br>
2023-11-10 09:35:11 -0300

    Aula 10/11

--- 4o-bimestre/criandoclasses/compra.js
+++ 4o-bimestre/criandoclasses/compra.js
@@ -5,6 +5,14 @@ class Usuario {
       this.nome = nome;
       this.email = email;
     }
+
+    adicionarUsuario(nome, email) {}
+    lerUsuario(id) {}
+    lerUsuarios() {}
+    atualizarUsuario(id, nome, email) {}
+    atualizarNomeUsuario(id, nome) {}
+    atualizarEmailUsuario(id, email) {}
+    deletarUsuario(id) {}
   }
   
   // Classe para a entidade Produto
@@ -14,8 +22,27 @@ class Usuario {
       this.nome = nome;
       this.preco = preco;
     }
+
+    criarProduto(nome, preco) {
+      // INSERT INTO produtos(nome, preco) VALUES (?,?)
+    }
+    lerProdutos() {
+      // SELECT * FROM produtos
+    }
+    lerProduto(id) {
+      // SELECT * FROM produtos WHERE ID=?
+    }
+    atualizarProduto(id, nome, preco) {
+      // UPDATE PRODUTO SET campo WHERE ID=?
+    }
+    atualizarNomeProduto(id, nome) {}
+    atualizarPrecoProduto(id, preco) {}
+
+    deletarProduto(id) {
+      // DELETE FROM produtos WHERE ID=?
+    }
+
   }
-  
   // Classe para o relacionamento Compra
   class Compra {
     constructor(id, data, quantidade, usuario, produto) {
@@ -25,5 +52,9 @@ class Usuario {
       this.usuario = usuario; // Associação com a entidade Usuario
       this.produto = produto; // Associação com a entidade Produto
     }
+
+
+
   }
   
+  let user1 = new Usuario(1, "david", "david.lima")
